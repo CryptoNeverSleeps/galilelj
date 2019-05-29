2fa1287fd74b83881816438cb0434d42967aa14c
babyface-oss <babyface@galilel.org>
2019-05-29 12:38:32 +0200

    Updated protocol version to latest 70722 for last fork changes

--- core/src/main/java/org/galilelj/core/CoinDefinition.java
+++ core/src/main/java/org/galilelj/core/CoinDefinition.java
@@ -73,8 +73,8 @@ public class CoinDefinition {
     public static final boolean feeCanBeRaised = false;
 
     // Galilel 3.2.0
-    public static final int PROTOCOL_VERSION = 70721;          // version.h PROTOCOL_VERSION
-    public static final int MIN_PROTOCOL_VERSION = 70721;      // version.h MIN_PROTO_VERSION
+    public static final int PROTOCOL_VERSION = 70722;          // version.h PROTOCOL_VERSION
+    public static final int MIN_PROTOCOL_VERSION = 70722;      // version.h MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT
     public static final int MAX_BLOCK_SIZE = 2 * 1000 * 1000;  // block.h MAX_BLOCK_SIZE_CURRENT
     public static final boolean supportsBloomFiltering = true; // Requires PROTOCOL_VERSION 70000 in the client
     public static final int Port = 36001;                      // protocol.h GetDefaultPort(testnet=false)
